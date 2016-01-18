7199a470028dea43450a3fa69b3ead8c76f099d8
dimdim4243 <dimdim4_243@mail.ru>
2016-01-18 08:23:22 +0300

    Добавление функций pars_primary, pars_add, pars_mull,  next_token, pars_uzel

--- main.cpp
+++ main.cpp
@@ -1,28 +1,76 @@
 using namespace std;
 #include "Lexser.h"
 
-struct node
+Token *cur;
+
+class Node
 {	
-    char info; //Информационное поле
-    node *left, *right;//Левая и Правая часть дерева
+protected:
+    string info; //Информационное поле
+    Node *left, *right;//Левая и Правая часть дерева
+public:
+	Node (string I, Node *L, Node *R){
+		info=I;
+		left=L;
+		right=R;
+	}
+	~Node(){}
 };
 
-node *tree=NULL;
+ Node *node; //начальная вершина дерева
+ Node *n;
+ string b;
 
-void makeNode(){
-	node childnode;
-	
+Lexser *l = new Lexser();
+void next_token(){
+	cur = l->get_token();
+}
 
+Node* pars_primary(){
+	 if (cur->tok == "ident" || cur->tok == "integer") {
+		 Node *t = new Node(cur->leks, NULL,  NULL);
+		next_token();
+		return node;
+	 }
+	 else {cout<<"Expected"; exit;}
+ }
+
+Node* pars_uzel(string info, Node *left, Node *right){
+	Node *t = new Node(cur->leks, left,  right);
+	return node;
+}
+
+void pars_mull(){
+	pars_primary();
+	string b = cur->leks;
+	while(cur->leks == "*" || cur->leks == "/"){
+		next_token();
+		n=pars_uzel(cur->leks, NULL, NULL);
+		next_token();
+		node=pars_uzel(b, n, NULL);
+	}
+}
+
+void pars_add(){
+	pars_primary();
+	string b = cur->leks;
+	cout<<b;
+	int k;
+	cin>>k;
+	while(b == "+" || b == "-"){
+		next_token();
+		n=pars_uzel(cur->leks, NULL, NULL);
+		next_token();
+		node=pars_uzel(b, n, NULL);
+	}
 }
 
 int main()
 {   
  	setlocale(LC_ALL,"Rus");
 	initmap();
-	Lexser *l = new Lexser();
-	fin >> noskipws;	
-	 Token *cur;
-	   try {
+	fin >> noskipws;
+	  /* try {
         while (cur = l->get_token()) {
 				if (tips!="coment" && tips!="per" && tips!=""){
             cur->print();
@@ -30,6 +78,10 @@ int main()
             delete cur;
         }
     }catch ( Errors *e) { e->print(); return 0;}
+	*/
+	
+	next_token();
+	pars_add();
 	fin.close();	
 	fout.close();	
 }
